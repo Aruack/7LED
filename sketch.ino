008f63bdd9ccaff8fbf977e59cbe31abc79f44a0
Aryan <83180468+Aruack@users.noreply.github.com>
2024-03-14 20:15:18 +0530

    ARUACK

---
+++ sketch.ino
@@ -0,0 +1,51 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(0, OUTPUT);
+pinMode(1, OUTPUT);
+pinMode(2, OUTPUT);
+pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(5, OUTPUT);
+pinMode(6, OUTPUT);
+pinMode(8, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(0, HIGH);
+delay(30);
+digitalWrite(0, LOW);
+delay(30);
+digitalWrite(1, HIGH);
+delay(40);
+digitalWrite(1, LOW);
+delay(40);
+digitalWrite(2, HIGH);
+delay(50);
+digitalWrite(2, LOW);
+delay(50);
+digitalWrite(3, HIGH);
+delay(60);
+digitalWrite(3, LOW);
+delay(60);
+digitalWrite(4, HIGH);
+delay(70);
+digitalWrite(4, LOW);
+delay(70);
+digitalWrite(5, HIGH);
+delay(80);
+digitalWrite(5, LOW);
+delay(80);
+digitalWrite(6, HIGH);
+delay(90);
+digitalWrite(6, LOW);
+delay(90);
+digitalWrite(8, HIGH);
+delay(100);
+digitalWrite(8, LOW);
+delay(100);
+
+
+
+
+}
